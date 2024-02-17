adc944e80f058e1e1eb8cf9cb45e56b573bb78f9
farhan-helmy <fhroslan@seekasia.com>
2024-02-17 14:35:35 +0800

    test: see nodeenv

--- src/app/scam/[id]/page.tsx
+++ src/app/scam/[id]/page.tsx
@@ -20,6 +20,7 @@ export async function generateMetadata({ params }: { params: { id: string } }): 
       : 'https://d8f7wymmosp6f.cloudfront.net/kenascamcover.png';
   const tags = data.scamToTags.map(tag => transformFormat(tag.tagId));
   const joinedTags = tags.join(',');
+  console.log(process.env.NODE_ENV)
   const ogImage = `https://${process.env.NODE_ENV === 'development' ? 'staging.' : ''}kenascam.xyz/og?title=${data.name}&description=${data.description}&authorName=anonymous&authorImage=${imageUrl}&tags=${joinedTags}`;
   return {
     title: `KenaScam | ${data.name}`,
