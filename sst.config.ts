7ea8919f2fca62024d2c8577df3965e825cb4c3e
farhan-helmy <fhroslan@seekasia.com>
2024-02-19 11:38:36 +0800

    feat: add custom domain to sst

--- sst.config.ts
+++ sst.config.ts
@@ -11,7 +11,9 @@ export default {
   },
   stacks(app) {
     app.stack(function Site({ stack }) {
-      const site = new NextjsSite(stack, 'site');
+      const site = new NextjsSite(stack, 'site', {
+        customDomain: app.stage === 'prod' ? 'kenascam.xyz' : 'staging.kenascam.xyz',
+      });
 
       stack.addOutputs({
         SiteUrl: site.url,
